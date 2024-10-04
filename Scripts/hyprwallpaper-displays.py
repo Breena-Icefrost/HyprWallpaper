5784d14d2c1d91ffeac4db623a3fe04fb472e199
Breena Stoneglove <143509797+Breena-Stoneglove@users.noreply.github.com>
2024-10-05 00:43:44 +0200

    Update hyprwallpaper-displays.py
    Changed xrandr to hyprctl monitors, as it's a better fit for Hyprland.

--- Scripts/hyprwallpaper-displays.py
+++ Scripts/hyprwallpaper-displays.py
@@ -1,8 +1,11 @@
 import subprocess
 
+# Define the command as a list of strings
+command = ['hyprctl', 'monitors']
+
 def screens():
-    output = [l for l in subprocess.check_output(["xrandr"]).decode("utf-8").splitlines()]
-    return [l.split()[0] for l in output if " connected " in l]
+    output = [l for l in subprocess.check_output(command).decode("utf-8").splitlines()]
+    return [l.split()[1] for l in output if "Monitor " in l]
 
 if __name__ == "__main__":
     displays = screens()
